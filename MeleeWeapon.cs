7271ee8e323970a375d24310451f3eeb7184f65b
hdbanks1 <hdbanks1@gmail.com>
2023-05-03 08:56:24 -0700

    Create MeleeWeapon.cs

---
+++ MeleeWeapon.cs
@@ -0,0 +1,11 @@
+Using UnityEngine;
+Using System.Serializable;
+
+public class MeleeWeapon:ScriptableObject{
+
+public int Damage;
+public animator Anim;
+
+Public void Start(){}
+Public void Update(){}
+}
